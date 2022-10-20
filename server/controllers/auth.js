9c86f0c72f8523ca19039aab442eff0a78bffcc1
Tina <wtina8998@gmail.com>
2022-10-19 21:52:59 -0600

    add forgetPassword

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,7 @@ const User = require('../models/user')
 const AWS = require('aws-sdk')
 const jwt = require('jsonwebtoken')
 const expressJWT = require('express-jwt')
-const {registerEmailParams} = require('../helpers/email')
+const {registerEmailParams,forgotPasswordEmailParams} = require('../helpers/email')
 const shortId = require('shortid')
 
 AWS.config.update({
@@ -142,6 +142,50 @@ exports.adminMiddleware = (req,res,next) => {
         req.profile = user
         next()
     })
+}
+
+exports.forgotPassword = (req,res) => {
+    const {email} = req.body
+    //check if user exists with that email
+    User.findOne({email}).exec((err,user) => {
+        if(err || !user){
+            return res.status(400).json({
+                error:"User with that email does not exist"
+            })
+        }
+        //generate token and email to user
+        const token = jwt.sign({name:user.name},process.env.JWT_RESET_PASSWORD,{expiresIn:'10m'})
+        //send email
+        const params = forgotPasswordEmailParams(email,token)
+
+        //populate the databse user to resetPasswordLink
+        return user.updateOne({resetPasswordLink:token},(err,success) => {
+            if(err){
+                return res.status(400).json({
+                    error:"Password reset failed. Try later"
+                })
+            }
+            const sendEmail = ses.sendEmail(params).promise()
+            sendEmail
+                .then(data => {
+                    console.log('ses reset password success',data)
+                    return res.json({
+                        message:`Email has been sent to ${email}. Click on the link to set your password`
+                    })
+                })
+                .catch(error => {
+                    console.log('ses reset password failed')
+                    return res.json({
+                        message:`We could not verify your email. Try later`
+                    })
+                })
+        })
+    })
+    
+}
+
+exports.resetPassword = (req,res) => {
+    
 }
 
 
