4ecf64abb690736f634563c1d26c12af217c4edc
Tina <wtina8998@gmail.com>
2022-10-29 23:48:24 -0600

    category create form

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,9 +1,29 @@
 import dynamic from 'next/dynamic'
-const Layout = dynamic(() => import('../../components/Layout'),{ssr:false})
+const Layout = dynamic(() => import('../../components/Layout'), { ssr: false })
 import withAdmin from '../withAdmin'
+import Link from 'next/link'
 
-const Admin= ({user,token}) => {
-    return <Layout>{JSON.stringify(user)}</Layout>
-  }
-  
-  export default withAdmin(Admin)
+const Admin = ({ user, token }) => {
+  return (
+    <Layout>
+      <h1>Admin Dashboard</h1>
+      <br />
+      <div className='row'>
+        <div className="col-md-4">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link href="/admin/category/create">
+                <a className='nav-link'>Create category</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-8">
+
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default withAdmin(Admin)
